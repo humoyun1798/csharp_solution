93ab14e28482549e1184c140c116a0fdd0db2c52
wanglei <1798460960@qq.com>
2024-04-23 23:53:41 +0800

    1

--- depend/Program.cs
+++ depend/Program.cs
@@ -1,27 +1,70 @@
-﻿namespace depend
+﻿
+using Microsoft.Extensions.DependencyInjection;
+
+namespace depend
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            var a = "123,232,344,222,322,55,1,23,5,7,944";
-            var item = a.Split(",");
-            var sum = 0;
-            for(int i = 0; i < item.Length; i++)
+
+            ServiceCollection services = new ServiceCollection();
+            //services.AddTransient<TestServiceImp1>(); //瞬时 GetService 一次创建一个对象
+            //services.AddSingleton<TestServiceImp1>();//单例
+
+            services.AddScoped<TestServiceImp1>();//范围
+
+            using (ServiceProvider sp = services.BuildServiceProvider()) //ServiceProvider ==服务定位器
             {
-                sum += int.Parse(item[i]);
+                /*
+                TestServiceImp1 t = sp.GetService<TestServiceImp1>();
+                t.Name = "lily0.";
+                t.SayHi();
+                TestServiceImp1 t1 = sp.GetService<TestServiceImp1>();
+                Console.WriteLine(object.ReferenceEquals(t1, t)); //判断两个对象是不是一个对象
+                */
+                using (IServiceScope scope1 = sp.CreateScope())
+                {
+                    //在 scope 中获取 对象要拿 scope里的 scope.xxx
+                    var t=scope1.ServiceProvider.GetService<TestServiceImp1>();
+
+                    t.Name = "lily0.";
+                    t.SayHi();
+                    var t1 = scope1.ServiceProvider.GetService<TestServiceImp1>();
+                    Console.WriteLine(object.ReferenceEquals(t1, t)); //判断两个对象是不是一个对象
+                }
+
+
+
             }
 
-            var avg=sum/item.Length;
 
 
-            var w = "hello World HHHHahahaha";
-            w=w.ToLower();
-            var list = w.GroupBy(w => w).Select(c => new { c.Key, c }).ToLsit();
-            Console.WriteLine(list);
+        }
+    }
 
-            Console.WriteLine($"Hello, World! {avg}");
+
+    public interface ITestService
+    {
+        public string Name { get; set; }
+        public void SayHi();
+    }
+
+    public class TestServiceImp1 : ITestService
+    {
+        public string Name { get; set; }
+        public void SayHi()
+        {
+            Console.WriteLine("TestServiceImp1 "+Name);
+        }
+    }
+    public class TestServiceImp2 : ITestService
+    {
+        public string Name { get; set; }
+        public void SayHi()
+        {
+            Console.WriteLine("TestServiceImp2");
         }
     }
 }
