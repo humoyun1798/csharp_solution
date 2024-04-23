f8bcfb92c354a9b42ad704df275a1cfec69d14a9
wanglei <1798460960@qq.com>
2024-04-23 17:32:14 +0800

    tesy

---
+++ depend/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace depend
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = "123,232,344,222,322,55,1,23,5,7,944";
+            var item = a.Split(",");
+            var sum = 0;
+            for(int i = 0; i < item.Length; i++)
+            {
+                sum += int.Parse(item[i]);
+            }
+
+            var avg=sum/item.Length;
+
+
+            var w = "hello World HHHHahahaha";
+            w=w.ToLower();
+            var list = w.GroupBy(w => w).Select(c => new { c.Key, c }).ToLsit();
+            Console.WriteLine(list);
+
+            Console.WriteLine($"Hello, World! {avg}");
+        }
+    }
+}
