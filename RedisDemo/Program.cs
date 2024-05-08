d4a05567bab4c81a444535a1b229ce9228d0158d
wanglei <1798460960@qq.com>
2024-05-08 14:52:05 +0800

    1

--- RedisDemo/Program.cs
+++ RedisDemo/Program.cs
@@ -13,7 +13,7 @@ namespace RedisDemo
             IDatabase db = redis.GetDatabase();
 
             // 存储数据
-            db.StringSet("myKey", "Hello, Redis!");
+            db.StringSet("myKey", "Hello, Redis!",TimeSpan.FromMinutes(10));
 
             // 读取数据
             string value = db.StringGet("myKey");
