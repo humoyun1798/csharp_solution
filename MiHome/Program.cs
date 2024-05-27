0a16644df9ba15e5fdaa3aa85aeb6c938872c084
wanglei <1798460960@qq.com>
2024-05-27 11:06:34 +0800

    update

---
+++ MiHome/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MiHome.Net;
+using MiHome.Net.Middleware;
+using MiHome.Net.Service;
+
+namespace MiHome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            All();
+            Console.WriteLine("Hello, World!");
+        }
+
+        static public async void All()
+        {
+           
+            //列出家庭里所有的智能家居设备
+            try
+            {
+                var miHomeDriver = CreateDriver();
+                var deviceList = miHomeDriver.Cloud.GetDeviceListAsync().Result;
+                //通过米家app里自己设置的智能家居名称找出自己想要操作的智能家居设备
+                var moonLight = deviceList.FirstOrDefault(it => it.Name == "月球灯");
+                //通过设备型号获取设备规格
+                var result = miHomeDriver.Cloud.GetDeviceSpec(moonLight.Model).Result;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+
+
+        static public IMiHomeDriver CreateDriver()
+        {
+            var hostBuilder = Host.CreateDefaultBuilder();
+            //添加小米米家的驱动服务，需要小米账号和密码
+            hostBuilder.ConfigureServices(it => it.AddMiHomeDriver(x =>
+            {
+                x.UserName = "17336335046";
+                x.Password = "wlgood55555";
+            }));
+            var host = hostBuilder.Build();
+            var miHomeDriver = host.Services.GetService<IMiHomeDriver>();
+            return miHomeDriver;
+        }
+
+
+
+    }
+}
---
+++ WebApi/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAA(int i)
+        {
+            if (i == 0)
+                return Ok(88);
+            else if (i == 1)
+                return Ok(555);
+            else
+                return NotFound("臭傻逼i传错了");
+
+        }
+    }
+}
