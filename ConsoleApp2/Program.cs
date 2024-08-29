e826c7cc078381e87be54c242eb2304266c7c4b8
wanglei <1798460960@qq.com>
2024-08-29 11:05:54 +0800

    cefbg

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            string input = "August 16, 2024";
+            DateTime date = DateTime.Parse(input);
+            string output = date.ToString("yyyy/MM/dd");
             Console.WriteLine("Hello, World!");
         }
     }
